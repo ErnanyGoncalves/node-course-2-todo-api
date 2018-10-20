4bc7fa2a0bec165e511624bc09ef9e3c52d9d7e9
Ernany <ernany@inatel.br>
2018-10-20 13:08:20 -0300

    Add update todos

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -12,18 +12,32 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true
 
     const db = client.db("TodoApp");
 
-    //deleteMany
-    /* db.collection("Todos").deleteMany({text:"Gozei"}).then((result)=>{
-        console.log(result);
-    }); */
-    //deleteOne
-    /* db.collection("Users").deleteOne({name:"Daniel"}).then((result)=>{
-        console.log(result);
-    }); */
-    //findOneAndDelete
-    /* db.collection("Todos").findOneAndDelete({completed:true}).then((result)=>{
+    /* db.collection("Todos").findOneAndUpdate({
+        _id: new ObjectID("5bcb502adbaeef6814239312")
+    }, {
+        $set:{
+            completed: true
+        }
+    }, {
+        returnOriginal: false
+    }).then((result)=>{
         console.log(result);
     }); */
     
+    db.collection("Users").findOneAndUpdate({
+        name: "Paul"
+    }, {
+        $set: {
+            location: "Status"
+        },
+        $inc:{
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result)=>{
+        console.log(result);
+    });
+
     //client.close();
 });
