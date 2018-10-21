dadf78e80d686452abafcbb082a3eaec699e14fa
Ernany <ernany@inatel.br>
2018-10-21 20:27:35 -0300

    GET /todos/:id added

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 var { mongoose } = require("./db/mongoose");
 var { Todo } = require("./models/todo");
@@ -14,18 +15,35 @@ app.post("/todos", (req, res) => {
         text: req.body.text
     });
 
-    todo.save().then((doc)=>{
+    todo.save().then((doc) => {
         res.send(doc);
-    }, (e)=>{
+    }, (e) => {
         res.status(400).send(e);
     });
 });
 
 app.get("/todos", (req, res) => {
-    Todo.find().then((todos)=>{
-        res.send({todos});
-    }, (e)=>{
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+app.get("/todos/:id", (req, res) => {
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+        res.send({todo});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
@@ -33,5 +51,5 @@ app.listen(3000, () => {
     console.log("Started on port 3000");
 });
 
-module.exports = {app};
+module.exports = { app };
 
